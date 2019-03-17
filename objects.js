2a2eec164f2f8d0fc357a45ffed6908e6331a0c9
Ay Has <ahashim983@yahoo.com>
2019-03-17 16:50:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,13 +3,9 @@ playlist.assign({Lady Gaga: Born This Way})
 
 function updatePlaylist(playlist, artist, song){
   playlist.assign({playlist},{artist : song})
-  
+
 }
 
 function removeFromPlaylist(playlist,artist){
-  delete 
+  delete
 }
-
-
-
-
